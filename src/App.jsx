f1d7202ea979c86905a40a4c2cfb2468449fb53a
vivek08wrk <vivekrawat08wrk@gamil.com>
2025-07-07 09:14:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import SearchBar from './components/SearchBar'
+import MovieCard from './components/MovieCard'
+
+function App() {
+  const [allMovieData, setAllMovieData] = useState([]);
+  const [searchMovie, setSearchMovie] =useState('');
+  const [loading, setLoading]= useState(false);
+
+  const fetchMovieData = async()=>{
+    try{
+        setLoading(true);
+        const res = await fetch(`http://www.omdbapi.com/?s=${searchMovie}&apikey=54a873d8`);
+        const data = await res.json();
+        setAllMovieData(data.Search);
+        console.log(data.Search);
+        setLoading(false);
+        
+
+    }catch(error){
+      console.log(error);
+      setLoading(false);
+
+    }
+  }
+  return (
+    <div>
+      <Navbar/>
+      <div className='bg'>
+        <SearchBar searchMovie={searchMovie}
+        setSearchMovie={setSearchMovie}
+        fetchMovieData={fetchMovieData}/>
+      <MovieCard allMovieData={allMovieData} loading={loading}/>
+      </div>
+    </div>
+  )
+}
+
+export default App
